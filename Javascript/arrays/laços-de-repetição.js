a1cd06ea286ccc0442925994d38833ab8e2d3a37
anaparastasi <137augusto@gmail.com>
2022-10-16 19:54:47 -0300

    curso Alura de JS

---
+++ Javascript/arrays/laços-de-repetição.js
@@ -0,0 +1,42 @@
+
+//-------------------- WHILE ----------------------------
+
+// const letraCerta = prompt('Digite a Letra A')
+
+// while(letraCerta !== A){
+//     letraCerta = prompt('Por favor, digite a Letra A')
+// }
+
+// alert('Obrigado!')
+
+// var total = 0
+
+// while(total <= 5){
+//     console.log(total)
+//     total++
+// }
+
+// console.log('saiu do while ' + total)
+
+//---------------------- FOR ---------------------------,
+
+
+// for (let i = 0; i <= 10; i++){
+    
+// console.log(i)
+
+// }
+
+const lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < lista.length; i++){
+    if(lista[i] ===3){
+        console.log('número encotrado')
+    }else{
+        console.log('tente novamente')
+    }
+
+    console.log(i)
+}
+
+ 
